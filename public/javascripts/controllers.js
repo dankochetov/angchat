6c658af9878c75bf6fdea10cbee8299f40719717
Danil Kochetov <danil.kochetov@gmail.com>
2015-09-14 22:43:32 +0300

    Added

--- public/javascripts/controllers.js
+++
@@ -1,5 +0,0 @@
-chatio.controller('initCtrl', function($scope, $location, auth){
-	$scope.init = function(user){
-		auth(user);
-	}
-});
--- public/javascripts/routes_chat.js
+++
@@ -1,6 +0,0 @@
-chatio.config(function($routeProvider){
-	$routeProvider.when('/', {
-		templateUrl: '/chat/default',
-		controller: 'chatDefaultCtrl'
-	});
-});
